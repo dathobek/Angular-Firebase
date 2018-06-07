33bed07784006736e28b9e67ebe189925756d152
David Athobe <dathobek@gmail.com>
2018-06-07 19:40:50 +0100

    Firebase initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
 
 
 import { AppComponent } from './app.component';
@@ -10,6 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    // initializing firebase.
+    AngularFireModule.initializeApp(environment.FirebaseConfig),
+    AngularFireDatabaseModule,
     BrowserModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  FirebaseConfig : {
+    
+      apiKey: "AIzaSyA9myDYhM3Ma8qM65neUKUcq9iaaJI2K2Y",
+      authDomain: "employeefire-39e81.firebaseapp.com",
+      databaseURL: "https://employeefire-39e81.firebaseio.com",
+      projectId: "employeefire-39e81",
+      storageBucket: "employeefire-39e81.appspot.com",
+      messagingSenderId: "440838990267"
+    
+  }
 };
