3a896f48c25f233060f37a50b64de22b2e6ddc5d
David Athobe <dathobek@gmail.com>
2018-06-18 01:22:13 +0100

    !Service

--- src/app/employees/Shared/employee.service.ts
+++ src/app/employees/Shared/employee.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import {AngularFireDatabase,AngularFireList} from 'angularfire2/database';
+import { Employee } from './employee.model'
 
 @Injectable()
 export class EmployeeService {
 
-  constructor() { }
+  employeelist: AngularFireList<any>;
+  selectedemployee: Employee = new Employee();
+
+  constructor(private firebase: AngularFireDatabase) { }
+
+  getData(){
+    this.employeelist = this.firebase.list('employees');
+    return this.employeelist;
+  }
 
 }
