f39583b1b7ba679683f6cf3d6ccaced0cad0a743
David Athobe <dathobek@gmail.com>
2018-06-18 01:34:08 +0100

    CRUD-executed

--- src/app/employees/Shared/employee.service.ts
+++ src/app/employees/Shared/employee.service.ts
@@ -15,4 +15,27 @@ export class EmployeeService {
     return this.employeelist;
   }
 
+  insertData(employee: Employee){
+    this.employeelist.push({
+      name: employee.name,
+      position: employee.position,
+      office: employee.office,
+      salary: employee.salary
+    });
+  }
+
+  updateData(employee: Employee){
+    this.employeelist.update(employee.$key,
+      {
+        name: employee.name,
+        position: employee.position,
+        office: employee.office,
+        salary: employee.salary
+    
+    });
+  }
+
+  deleteData($key: string){
+    this.employeelist.remove($key)
+  }
 }
