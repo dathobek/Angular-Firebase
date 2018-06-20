ebfbe893c2a269e920c1ad24c8791726d15bae3d
David Athobe <dathobek@gmail.com>
2018-06-20 08:24:50 +0100

    err-Fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,7 @@ import { NgModule } from '@angular/core';
 import {AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
-=======
->>>>>>> 3f6eb4d3c28741a62e22a4418a5ee7b58ee505d6
-=======
->>>>>>> 3f6eb4d3c28741a62e22a4418a5ee7b58ee505d6
-
-
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
@@ -29,16 +21,12 @@ import { EmployeeListComponent } from './employees/employee-list/employee-list.c
     // initializing firebase.
     AngularFireModule.initializeApp(environment.FirebaseConfig),
     AngularFireDatabaseModule,
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
     BrowserModule,
-    FormsModule
-=======
+    FormsModule,
     BrowserModule
->>>>>>> 3f6eb4d3c28741a62e22a4418a5ee7b58ee505d6
-=======
-    BrowserModule
->>>>>>> 3f6eb4d3c28741a62e22a4418a5ee7b58ee505d6
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
