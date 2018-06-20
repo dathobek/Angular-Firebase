3af3e6b5d976b31ac71362dfe78d31bb5b1cff5d
David Athobe <dathobek@gmail.com>
2018-06-20 09:50:15 +0100

    reset-Button

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../Shared/employee.service';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-employee',
@@ -11,6 +12,23 @@ export class EmployeeComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
+  onSubmit(employeeForm : NgForm){
+
+       }
+
+  resetForm(employeeForm? : NgForm){
+    if(employeeForm != null)
+    employeeForm.reset();
+    this.employeeService.selectedemployee = {
+       $key : null,
+       name : '',
+       position : '',
+       office : '',
+       salary : 0
+    }
+
+       }     
 }
